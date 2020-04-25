51c3e6fdf754b49ae1541d27799380ed3bdddb22
nimuC <nimuC@outlook.com>
2020-04-25 12:28:13 +0900

    Rename @mark to @char_mark

--- lib/player.rb
+++ lib/player.rb
@@ -2,12 +2,14 @@
 
 module TicTacToe
   class Player
-    def initialize(mark)
-      @mark = mark
+    attr_reader :char_mark
+
+    def initialize(char_mark)
+      @char_mark = char_mark
     end
 
     def mark(board, point_x, point_y)
-      board.mark(point_x, point_y, @mark)
+      board.mark(point_x, point_y, @char_mark)
     end
   end
 end
